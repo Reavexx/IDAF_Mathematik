c653760e2d39544365ed09c25c6a417318722867
Dines Nimalthas <Dinesnimal@gmail.com>
2023-01-24 14:35:45 +0100

    Grid Veränderungen

--- main.py
+++ main.py
@@ -11,33 +11,33 @@ def calculate():
     result_label.config(text=f"x1 = {x1}\nx2 = {x2}")
 
 root = tk.Tk()
-root.title("Mitternachtsformel made by Dines Nimalthas")
+root.title("Mitternachtsformel Berechner made by Dines Nimalthas")
 
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(index=0, weight=1)
+root.grid_rowconfigure(index=0, weight=1)
 
 a_label = tk.Label(root, text="a:")
-a_label.grid(row=0, column=0)
+a_label.grid(row=0, column=0, sticky='nwse')
 
 a_entry = tk.Entry(root)
-a_entry.grid(row=0, column=1)
+a_entry.grid(row=0, column=1, sticky='nwse')
 
 b_label = tk.Label(root, text="b:")
-b_label.grid(row=1, column=0)
+b_label.grid(row=1, column=0, sticky='nwse')
 
 b_entry = tk.Entry(root)
-b_entry.grid(row=1, column=1)
+b_entry.grid(row=1, column=1, sticky='nwse')
 
 c_label = tk.Label(root, text="c:")
-c_label.grid(row=2, column=0)
+c_label.grid(row=2, column=0, sticky='nwse')
 
 c_entry = tk.Entry(root)
-c_entry.grid(row=2, column=1)
+c_entry.grid(row=2, column=1, sticky='nwse')
 
 calculate_button = tk.Button(root, text="Berechnen", command=calculate)
-calculate_button.grid(row=3, column=0, columnspan=2)
+calculate_button.grid(row=3, column=0, columnspan=2, sticky='nwse')
 
 result_label = tk.Label(root)
-result_label.grid(row=4, column=0, columnspan=2)
+result_label.grid(row=4, column=0, columnspan=2, sticky='nwse')
 
 root.mainloop()
