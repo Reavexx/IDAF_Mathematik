fe5d53b6cf232f54fc5c99419033b622ff927a0b
Dines Nimalthas <dines_nimalthas@sluz.ch>
2023-04-26 10:22:04 +0200

    Added vertex formula

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import messagebox
 
 # Formula for Midnight
+
+
 def calculate():
     a = float(a_entry.get())
     b = float(b_entry.get())
@@ -12,43 +14,43 @@ def calculate():
     x2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
     diskriminate = b**2 - 4*a*c
 
-    if(x1 == x2):
+    if (x1 == x2):
         result_label.config(text=f"x = {x1}\nDoppellösung")
         messagebox.showinfo("Lösung?", "Doppellösung")
     elif (diskriminate < 0):
         result_label.config(text=f"Keine lösung")
         messagebox.showinfo("Lösung?", "Keine Lösung")
     elif (x1 != x2):
-        result_label.config(text=f"x1 = {x1}\nx2 = {x2}")
+        result_label.config(
+            text=f"x1 = {x1}\nx2 = {x2}\nScheitelpunkt: ({-b/(2*a)}, {-(b**2 - 4*a*c)/(4*a)})")
+
 
 root = tk.Tk()
 root.title("Mitternachtsformel Berechner")
-
-
 
 Title_label = tk.Label(root, text="Mitternachtsformel Berechner")
 Title_label.pack(side="top")
 
 a_label = tk.Label(root, text="a:")
-a_label.pack(side="left", padx= 15, pady=15)
+a_label.pack(side="left", padx=15, pady=15)
 
 a_entry = tk.Entry(root)
-a_entry.pack(side="left", padx= 15, pady=15)
+a_entry.pack(side="left", padx=15, pady=15)
 
 b_label = tk.Label(root, text="b:")
-b_label.pack(side="left", padx= 15, pady=15)
+b_label.pack(side="left", padx=15, pady=15)
 
 b_entry = tk.Entry(root)
-b_entry.pack(side="left", padx= 15, pady=15)
+b_entry.pack(side="left", padx=15, pady=15)
 
 c_label = tk.Label(root, text="c:")
-c_label.pack(side="left", padx= 15, pady=15)
+c_label.pack(side="left", padx=15, pady=15)
 
 c_entry = tk.Entry(root)
-c_entry.pack(side="left", padx= 15, pady=15)
+c_entry.pack(side="left", padx=15, pady=15)
 
 calculate_button = tk.Button(root, text="Berechnen", command=calculate)
-calculate_button.pack(side="left", padx= 15, pady=15)
+calculate_button.pack(side="left", padx=15, pady=15)
 
 result_label = tk.Label(root)
 result_label.pack(side="left")
